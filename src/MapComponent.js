847b4b7be2871676bdd11e3f04b6b6894488ac78
Dung Nguyen <dungqnguyen1506@gmail.com>
2020-04-06 10:33:17 -0700

    test

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,18 +1,30 @@
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import {usePosition} from './usePosition';
+import { geolocated, geoPropTypes } from "react-geolocated";
+import ReactDOM from 'react-dom';
 
 const mapStyles = {
     width: '100%',
     height: '100%',
   };
-  
+ 
+//const {latitude, longitude, error} = usePosition();
+
 class MapComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            Lat : null,
+            Lng : null
+        };
+    }
+
     render() {
         return (
             <Map
               google={this.props.google}
-              zoom={8}
+              zoom={12}
               style={mapStyles}
               initialCenter={{ lat: 47.444, lng: -122.176}}
             />
@@ -20,8 +32,17 @@ class MapComponent extends Component{
       }
     
 }
+// Using ES6 object spread syntax
+MapComponent.propTypes = { ...MapComponent.propTypes, ...geoPropTypes };
+const MainWithGeoloc = geolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  })(MapComponent);
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyC1tGOt9uH0XObaoIGi8djUckDpjMV2SEY'
   })(MapComponent);
   
+ReactDOM.render(<MainWithGeoloc />, document.getElementById('root'));
 
